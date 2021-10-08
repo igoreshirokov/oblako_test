176f4578bf541f3a80dfdc2df7070dd204416934
root <root@DESKTOP-Q29LB6N.localdomain>
2021-10-08 07:49:59 +0300

    +headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
+    before_action :custom_headers
+
+    def custom_headers
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'POST, PATCH, GET'
+      response.headers['Access-Control-Request-Method'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      response.headers["Content-Type"] = "application/json"
+    end
 end
